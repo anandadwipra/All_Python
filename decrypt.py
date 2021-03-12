e8639f1b2228f8726714e7f3c66f8f9375b45828
AnandaDwiPra <48857662+anandadwipra@users.noreply.github.com>
2021-03-12 11:33:00 +0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -12,3 +12,30 @@ ValueError: Strings for DES must be a multiple of 8 in length
 '\021,\343Nq\214DY\337T\342pA\372\255\311s\210\363,\300j\330\250\312\347\342I\3215w\03561\303dgb/\006'
 >>> obj.decrypt(ciph)
 'Guido van Rossum is a space alien.XXXXXX'
+
+
+------------------------
+
+from enigma.machine import EnigmaMachine
+
+# setup machine according to specs from a daily key sheet:
+
+machine = EnigmaMachine.from_key_sheet(
+       rotors='II IV V',
+       reflector='B',
+       ring_settings=[1, 20, 11],
+       plugboard_settings='AV BS CG DL FU HZ IN KM OW RX')
+
+# set machine initial starting position
+machine.set_display('WXC')
+
+# decrypt the message key
+msg_key = machine.process_text('KCH')
+
+# decrypt the cipher text with the unencrypted message key
+machine.set_display(msg_key)
+
+ciphertext = 'NIBLFMYMLLUFWCASCSSNVHAZ'
+plaintext = machine.process_text(ciphertext)
+
+print(plaintext)
